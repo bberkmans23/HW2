ae7473666fb83f2ab03421b1ce00a334bce9ab03
bberkmans23 <bberkmans@luc.edu>
2025-09-23 22:31:55 -0500

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,6 +1,6 @@
 
 /*
- * *** PLACE YOUR NAME / SECTION HERE ***
+ * *** Brendan Berkmans / 272 002 ***
  *
  * Homework # 2 (Programming Assignment). This Java class defines a few basic
  * manipulation operations of a binary trees.
